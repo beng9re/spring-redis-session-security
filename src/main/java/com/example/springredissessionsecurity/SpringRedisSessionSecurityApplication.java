f3542e79bbc6f65f06b37c7923671c4745107adf
kbh0581 <kbh0581@gmail.com>
2022-05-03 23:56:07 +0900

    Initial commit

---
+++ src/main/java/com/example/springredissessionsecurity/SpringRedisSessionSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springredissessionsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisSessionSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisSessionSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springredissessionsecurity/SpringRedisSessionSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springredissessionsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisSessionSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
