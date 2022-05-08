0fd14ddc1f5df5f962611d7c2f7bb2f0a62bfb7d
kbh0581 <kbh0581@gmail.com>
2022-05-08 23:05:07 +0900

    feat : redis config 추가

---
+++ src/main/java/com/example/springredissessionsecurity/instructure/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.example.springredissessionsecurity.instructure;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig extends AbstractHttpSessionApplicationInitializer {
+    @Value("${spring.redis.host}")
+    public String host;
+
+    @Value("${spring.redis.port}")
+    public int port;
+
+    @Bean
+    public RedisConnectionFactory connectionFactory(){
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        return new LettuceConnectionFactory(configuration);
+    }
+}
